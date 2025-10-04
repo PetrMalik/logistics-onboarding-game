0b1e1dcc653a60f2cb424076129ab0c384539375
Frankie Pernicka <fpernicka@paylocity.com>
2025-10-04 12:54:26 +0200

    uprava mapy

--- src/components/Environment.tsx
+++ src/components/Environment.tsx
@@ -138,7 +138,7 @@ function ChecklistItem({
 export function Environment() {
   return (
     <group>
-      {/* Stromy - rozšířené rozmístění po celé ploše 200x200 */}
+      {/* Stromy - přemístěné mimo silnice na ploše 200x200 */}
       <Tree position={[-8, 0, 5]} />
       <Tree position={[-10, 0, -3]} />
       <Tree position={[7, 0, -8]} />
@@ -146,16 +146,16 @@ export function Environment() {
       <Tree position={[-15, 0, 12]} />
       <Tree position={[12, 0, 15]} />
       <Tree position={[-6, 0, -12]} />
-      {/* Původní rozšířené stromy */}
-      <Tree position={[-25, 0, 20]} />
+      {/* Přemístěné stromy mimo silnice */}
+      <Tree position={[-33, 0, 20]} />  {/* z -25,0,20 - mimo silnici X=-25 */}
       <Tree position={[-30, 0, -15]} />
       <Tree position={[-35, 0, 5]} />
-      <Tree position={[-20, 0, -25]} />
+      <Tree position={[-20, 0, -33]} />  {/* z -20,0,-25 - mimo silnici Z=-25 */}
       <Tree position={[-40, 0, 30]} />
-      <Tree position={[25, 0, -20]} />
+      <Tree position={[33, 0, -20]} />   {/* z 25,0,-20 - mimo silnici X=25 */}
       <Tree position={[30, 0, 10]} />
       <Tree position={[35, 0, -5]} />
-      <Tree position={[20, 0, 25]} />
+      <Tree position={[20, 0, 33]} />    {/* z 20,0,25 - mimo silnici Z=25 */}
       <Tree position={[40, 0, -30]} />
       <Tree position={[-18, 0, 40]} />
       <Tree position={[18, 0, -40]} />
@@ -167,20 +167,20 @@ export function Environment() {
       <Tree position={[-38, 0, -20]} />
       <Tree position={[15, 0, 45]} />
       <Tree position={[-15, 0, -45]} />
-      {/* Nové stromy pro mapu 200x200 */}
+      {/* Nové stromy pro mapu 200x200 - přemístěné mimo silnice */}
       <Tree position={[-60, 0, 40]} />
       <Tree position={[-70, 0, -30]} />
-      <Tree position={[-80, 0, 10]} />
-      <Tree position={[-50, 0, -60]} />
+      <Tree position={[-88, 0, 10]} />   {/* z -80,0,10 - mimo silnici X=-75 */}
+      <Tree position={[-55, 0, -60]} />  {/* z -50,0,-60 - mimo silnici X=-50 */}
       <Tree position={[-90, 0, 70]} />
-      <Tree position={[60, 0, -50]} />
-      <Tree position={[70, 0, 25]} />
-      <Tree position={[80, 0, -15]} />
-      <Tree position={[50, 0, 65]} />
+      <Tree position={[60, 0, -55]} />   {/* z 60,0,-50 - mimo silnici Z=-50 */}
+      <Tree position={[70, 0, 33]} />    {/* z 70,0,25 - mimo silnici Z=25 */}
+      <Tree position={[88, 0, -15]} />   {/* z 80,0,-15 - mimo silnici X=75 */}
+      <Tree position={[55, 0, 65]} />    {/* z 50,0,65 - mimo silnici X=50 */}
       <Tree position={[90, 0, -70]} />
       <Tree position={[-55, 0, 85]} />
       <Tree position={[55, 0, -85]} />
-      <Tree position={[-95, 0, -25]} />
+      <Tree position={[-95, 0, -33]} />  {/* z -95,0,-25 - mimo silnici Z=-25 */}
       <Tree position={[95, 0, 35]} />
       <Tree position={[65, 0, 80]} />
       <Tree position={[-65, 0, -80]} />
@@ -188,48 +188,48 @@ export function Environment() {
       <Tree position={[-85, 0, -45]} />
       <Tree position={[35, 0, 95]} />
       <Tree position={[-35, 0, -95]} />
-      <Tree position={[75, 0, -75]} />
-      <Tree position={[-75, 0, 75]} />
-      <Tree position={[25, 0, -90]} />
-      <Tree position={[-25, 0, 90]} />
-      <Tree position={[90, 0, 5]} />
-      <Tree position={[-90, 0, -5]} />
+      <Tree position={[88, 0, -88]} />   {/* z 75,0,-75 - mimo silnice X=75,Z=-75 */}
+      <Tree position={[-88, 0, 88]} />   {/* z -75,0,75 - mimo silnice X=-75,Z=75 */}
+      <Tree position={[33, 0, -90]} />   {/* z 25,0,-90 - mimo silnici X=25 */}
+      <Tree position={[-33, 0, 90]} />   {/* z -25,0,90 - mimo silnici X=-25 */}
+      <Tree position={[90, 0, 8]} />     {/* z 90,0,5 - mimo blízko silnic */}
+      <Tree position={[-90, 0, -8]} />   {/* z -90,0,-5 - mimo blízko silnic */}
 
-      {/* Budovy - rozšířené rozmístění po celé ploše 200x200 */}
+      {/* Budovy - přemístěné mimo silnice na ploše 200x200 */}
       <Building position={[-12, 0, -10]} width={3} height={4} depth={3} color="#F5E6D3" />
       <Building position={[10, 0, -15]} width={2.5} height={5} depth={2.5} color="#FFE4C4" />
       <Building position={[-18, 0, 8]} width={2} height={3} depth={2} color="#F8E8D8" />
       <Building position={[15, 0, 10]} width={3.5} height={3.5} depth={3} color="#F5DEB3" />
-      {/* Původní rozšířené budovy */}
-      <Building position={[-25, 0, -20]} width={2.8} height={4.2} depth={2.8} color="#FFF8DC" />
-      <Building position={[28, 0, -25]} width={3.2} height={3.8} depth={3.2} color="#F5E6D3" />
+      {/* Přemístěné budovy mimo silnice */}
+      <Building position={[-33, 0, -20]} width={2.8} height={4.2} depth={2.8} color="#FFF8DC" />  {/* z -25,0,-20 - mimo X=-25 */}
+      <Building position={[28, 0, -33]} width={3.2} height={3.8} depth={3.2} color="#F5E6D3" />   {/* z 28,0,-25 - mimo Z=-25 */}
       <Building position={[-35, 0, 15]} width={2.2} height={4.5} depth={2.2} color="#FFE4C4" />
       <Building position={[32, 0, 18]} width={3.8} height={3.2} depth={3.8} color="#F8E8D8" />
       <Building position={[-28, 0, -35]} width={2.5} height={5.2} depth={2.5} color="#F5DEB3" />
       <Building position={[35, 0, -35]} width={3.0} height={4.0} depth={3.0} color="#FFF8DC" />
-      <Building position={[-40, 0, 25]} width={2.3} height={3.8} depth={2.3} color="#F5E6D3" />
+      <Building position={[-40, 0, 33]} width={2.3} height={3.8} depth={2.3} color="#F5E6D3" />    {/* z -40,0,25 - mimo Z=25 */}
       <Building position={[38, 0, 28]} width={3.5} height={4.2} depth={3.5} color="#FFE4C4" />
       <Building position={[-32, 0, 40]} width={2.7} height={3.5} depth={2.7} color="#F8E8D8" />
-      <Building position={[25, 0, 42]} width={3.3} height={4.8} depth={3.3} color="#F5DEB3" />
+      <Building position={[33, 0, 42]} width={3.3} height={4.8} depth={3.3} color="#F5DEB3" />     {/* z 25,0,42 - mimo X=25 */}
       <Building position={[-45, 0, -12]} width={2.4} height={3.6} depth={2.4} color="#FFF8DC" />
       <Building position={[42, 0, -18]} width={3.6} height={4.4} depth={3.6} color="#F5E6D3" />
       <Building position={[20, 0, -42]} width={2.6} height={5.0} depth={2.6} color="#FFE4C4" />
       <Building position={[-22, 0, 45]} width={3.1} height={3.3} depth={3.1} color="#F8E8D8" />
-      {/* Nové budovy pro mapu 200x200 */}
+      {/* Nové budovy pro mapu 200x200 - přemístěné mimo silnice */}
       <Building position={[-60, 0, -45]} width={3.4} height={4.6} depth={3.4} color="#F5E6D3" />
-      <Building position={[65, 0, -55]} width={2.9} height={5.1} depth={2.9} color="#FFE4C4" />
-      <Building position={[-75, 0, 30]} width={2.7} height={3.9} depth={2.7} color="#F8E8D8" />
+      <Building position={[65, 0, -58]} width={2.9} height={5.1} depth={2.9} color="#FFE4C4" />    {/* z 65,0,-55 - mimo Z=-50 */}
+      <Building position={[-88, 0, 30]} width={2.7} height={3.9} depth={2.7} color="#F8E8D8" />    {/* z -75,0,30 - mimo X=-75 */}
       <Building position={[70, 0, 35]} width={4.0} height={4.3} depth={4.0} color="#F5DEB3" />
-      <Building position={[-55, 0, -70]} width={3.1} height={5.5} depth={3.1} color="#FFF8DC" />
-      <Building position={[80, 0, -75]} width={3.6} height={4.1} depth={3.6} color="#F5E6D3" />
-      <Building position={[-85, 0, 50]} width={2.8} height={4.7} depth={2.8} color="#FFE4C4" />
-      <Building position={[75, 0, 55]} width={3.9} height={3.7} depth={3.9} color="#F8E8D8" />
+      <Building position={[-58, 0, -70]} width={3.1} height={5.5} depth={3.1} color="#FFF8DC" />   {/* z -55,0,-70 - mimo X=-50 */}
+      <Building position={[80, 0, -88]} width={3.6} height={4.1} depth={3.6} color="#F5E6D3" />    {/* z 80,0,-75 - mimo Z=-75 */}
+      <Building position={[-85, 0, 58]} width={2.8} height={4.7} depth={2.8} color="#FFE4C4" />    {/* z -85,0,50 - mimo Z=50 */}
+      <Building position={[88, 0, 55]} width={3.9} height={3.7} depth={3.9} color="#F8E8D8" />     {/* z 75,0,55 - mimo X=75 */}
       <Building position={[-70, 0, 80]} width={3.2} height={5.3} depth={3.2} color="#F5DEB3" />
       <Building position={[85, 0, 85]} width={3.7} height={4.9} depth={3.7} color="#FFF8DC" />
       <Building position={[-90, 0, -30]} width={2.5} height={4.2} depth={2.5} color="#F5E6D3" />
       <Building position={[90, 0, -35]} width={4.1} height={3.8} depth={4.1} color="#FFE4C4" />
       <Building position={[45, 0, 90]} width={3.3} height={5.4} depth={3.3} color="#F8E8D8" />
-      <Building position={[-50, 0, 85]} width={2.6} height={4.4} depth={2.6} color="#F5DEB3" />
+      <Building position={[-58, 0, 85]} width={2.6} height={4.4} depth={2.6} color="#F5DEB3" />    {/* z -50,0,85 - mimo X=-50 */}
       <Building position={[95, 0, -15]} width={3.8} height={4.8} depth={3.8} color="#FFF8DC" />
       <Building position={[-95, 0, 20]} width={3.0} height={3.6} depth={3.0} color="#F5E6D3" />
 
